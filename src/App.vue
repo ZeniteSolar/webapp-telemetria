ced19e7b766a08ee29df809eac28809ce4ce7c98
Vinicius Cardoso <viniciuscc10@gmail.com>
2019-12-22 22:35:51 -0300

    v0.0.1 - Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Login" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            append-icon="mdi-eye-off"
+            @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text color="blue">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      showPassword: false
+    };
+  }
+};
+</script>
