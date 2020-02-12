25c1d152c715a23533ba49a201e883ce1c6fafd3
Vinicius Cardoso <viniciuscc10@gmail.com>
2020-02-12 01:45:09 -0300

    v0.1.7 - Add filter on log page

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -50,45 +50,35 @@ export default {
   data: () => ({
     items: [
       {
-        icon: "mdi-bookmark-multiple",
-        text: "Marcadores",
-        link: "/marcadores" },
+        icon: "mdi-home-variant",
+        text: "Home",
+        link: "/"
+      },
       {
-        icon: "mdi-console",
-        text: "Log de Dados",
-        link: "/log"
+        icon: "mdi-view-dashboard",
+        text: "Dashboard",
+        link: "/dash"
+      },
+      {
+        icon: "mdi-chart-timeline-variant",
+        text: "Gráficos",
+        link: "/grafico"
       },
       {
         icon: "mdi-sim",
         text: "Módulos e Tópicos",
         link: "/modulos"
+      },
+      {
+        icon: "mdi-bookmark-multiple",
+        text: "Marcadores",
+        link: "/marcadores"
+      },
+      {
+        icon: "mdi-console",
+        text: "Log de Dados",
+        link: "/log"
       }
-
-      // {
-      //   icon: "mdi-chevron-up",
-      //   "icon-alt": "mdi-chevron-down",
-      //   text: "Boat Modules",
-      //   model: false,
-      //   children: [{ icon: "mdi-battery-80", text: "Baterry" }]
-      // }
-      // {
-      //   icon: "mdi-chevron-up",
-      //   "icon-alt": "mdi-chevron-down",
-      //   text: "More",
-      //   model: false,
-      //   children: [
-      //     { text: "Import" },
-      //     { text: "Export" },
-      //     { text: "Print" },
-      //     { text: "Undo changes" },
-      //     { text: "Other contacts" }
-      //   ]
-      // },
-      // { icon: "mdi-settings", text: "Settings" },
-      // { icon: "mdi-message", text: "Send feedback" },
-      // { icon: "mdi-help-circle", text: "Help" },
-      // { icon: "mdi-cellphone-link", text: "App downloads" },
-      // { icon: "mdi-keyboard", text: "Go to the old version" }
     ]
   })
 };
--- src/pages/Log.vue
+++ src/pages/Log.vue
@@ -3,15 +3,36 @@
       <v-col cols="12">
         <v-card>
           <v-card-title>
-            Log de Dados
-            <v-spacer></v-spacer>
             <v-text-field
+            label="Módulo"
+            v-model="filter.mod"
+          ></v-text-field>
+          <v-text-field
+            label="Tópico"
+            v-model="filter.top"
+          ></v-text-field>
+          <v-text-field
+            label="Hora Mínima"
+            v-model="filter.min_time"
+          ></v-text-field>
+          <v-text-field
+            label="Hora Máxima"
+            v-model="filter.max_time"
+          ></v-text-field>
+          <v-text-field
+            label="Limite"
+            v-model="filter.limit"
+          ></v-text-field>
+          <v-btn class="ma-2" outlined color="teal" @click="updateLog">Filtrar</v-btn>
+          
+    
+            <!-- <v-text-field
               v-model="search"
               append-icon="mdi-magnify"
               label="Search"
               single-line
               hide-details
-            ></v-text-field>
+            ></v-text-field> -->
           </v-card-title>
           <v-data-table
             :headers="headers"
@@ -34,27 +55,6 @@ import Api from '@/services/api';
 export default {
   name: "Data",
   components: {},
-  mounted() {
-
-    let mod_params = this.$route.params.mod;
-    let top_params = this.$route.params.top;
-    let url = "/data/";
-
-    if( mod_params == null){
-      url = "/data/";
-    }else{
-      if (top_params == null){
-        url = "/data/"+mod_params;
-      }else{
-        url = "/data/"+mod_params+"/"+top_params;
-      }
-    }
-
-    Api().get(url).then((data)=>{
-        this.data = data.data;
-      });
-    
-  },
   data() {
     return {
       search: "",
@@ -72,6 +72,13 @@ export default {
       ],
       data: [],
       type: [],
+      filter: {
+        mod: "",
+        top: "",
+        min_time: "",
+        max_time: "",
+        limit: ""
+      },
       breadcrumbs: [
         {
           text: 'Início',
@@ -86,5 +93,42 @@ export default {
       ],
     };
   },
+  methods : {
+    updateLog(){
+      // this.data.data = [];
+      // let params = "";
+
+      
+
+      let mod = ( this.filter.mod != "" ) ? "mod="+this.filter.mod : "";
+      let top = ( this.filter.top != "" ) ? "top="+this.filter.top : "";
+      let min_time = ( this.filter.min_time != "" ) ? "date>"+this.filter.min_time : "";
+      let max_time = ( this.filter.max_time != "" ) ? "date<"+this.filter.max_time : "";
+      let limit = ( this.filter.limit != "" ) ? "limit="+this.filter.limit : "";
+
+    
+      let url = "/data/" + "?" + mod + "&" + top + "&" + min_time + "&" + max_time + "&" + limit;
+
+      // if( mod_params == null){
+      //   url = "/data/";
+      // }else{
+      //   if (top_params == null){
+      //     url = "/data/"+mod_params;
+      //   }else{
+      //     url = "/data/"+mod_params+"/"+top_params;
+      //   }
+      // }
+
+      Api().get(url).then((data)=>{
+          this.data = data.data;
+      });
+
+      
+
+    } 
+  },
+  mounted() {
+    this.updateLog();
+  }
 };
 </script>
